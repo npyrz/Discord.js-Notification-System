5af597573203c2bce06c4e1af9298e828469782d
Noah <neoptunium@gmail.com>
2021-03-15 16:24:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({
+  disableEveryone: true
+});
+
+var stickyMessage = null;
+
+bot.on('message', message => {
+  var messageContent = message.content;
+  if (message.member != null) {
+     if (message.member.hasPermission("ADMINISTRATOR")) {
+        if (messageContent.split(" ")[0].charAt(0) === '!') {
+           var command = messageContent.split(" ");
+           switch (command[0].toLowerCase()) {
+              case "!sticky":
+                 var message = "__**⚠️ Stickied Message ⚠️:**__ ";
+                 for (var i = 1; i < command.length; i++) {
+                    message += command[i] + " ";
+                 }
+                 message.channel.send(message).then(message => {
+                    stickyMessage = message;
+                 });
+                 message.delete();
+                 break;
+              case "!unsticky":
+                 message.delete();
+                 stickyMessage.delete();
+                 stickyMessage = null;
+                 break;
+              default:
+           }
+        }
+     }
+  }
+  if (stickyMessage != null && message.content !== stickyMessage.content && message.channel === stickyMessage.channel) {
+     message.channel.send(stickyMessage.content)
+        .then(message => {
+           stickyMessage.delete();
+           stickyMessage = message;
+        });
+  }
+
+});
+
+bot.login();
